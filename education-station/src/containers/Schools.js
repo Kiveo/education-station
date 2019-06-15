4adc69b0f0a61aec2a178cf07c7e8f11fd50050e
Mav <mavericktesini@gmail.com>
2019-06-15 16:11:14 -0400

    npm ci after errors and setup eslint rules

--- education-station/src/containers/Schools.js
+++ education-station/src/containers/Schools.js
@@ -1,55 +1,52 @@
 import React, { Component } from 'react';
-import { Header, Loader, Title, WelcomeMessage } from '../components/content';
+import { connect } from 'react-redux';
+import {
+  Header, Loader, Title, WelcomeMessage,
+} from '../components/content';
 import School from '../components/School';
 import './Schools.css';
 
-import { connect } from 'react-redux';
-
 class Schools extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoading: false,
-    }
+    };
   }
 
   render() {
     const { isLoading } = this.state;
     const { schools } = this.props;
-    const renderSchools = schools ? schools.map((sch) => (
+    const renderSchools = schools ? schools.map(sch => (
       <li>
-      <School 
-        school={sch}
+        <School
+          school={sch}
         />
       </li>
-    )) : 'Not available'
-    
-    console.log('plural: ', schools)
+    )) : 'Not available';
+
+    console.log('plural: ', schools);
     return (
-        <div className="schools">
-          <section className="schools-section">
-            <Header header='Education Station' />
-            <WelcomeMessage welcomeClasses="schools-welcome" />
-          </section>
-          <section className="schools-section" id='school-list'> 
-            <Title title='Schools'/>
-            {isLoading ? 
-              <Loader />
-              : renderSchools
+      <div className="schools">
+        <section className="schools-section">
+          <Header header="Education Station" />
+          <WelcomeMessage welcomeClasses="schools-welcome" />
+        </section>
+        <section className="schools-section" id="school-list">
+          <Title title="Schools" />
+          {isLoading
+            ? <Loader />
+            : renderSchools
             }
-          </section>
-        </div>
-      
-    )
+        </section>
+      </div>
+
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return { schools: state.schools }  
-}
+const mapStateToProps = state => ({ schools: state.schools });
 
-const mapDispatchToProps = dispatch => {
-  return (dispatch('action'))
-};
+const mapDispatchToProps = dispatch => (dispatch('action'));
 
 export default connect(mapStateToProps)(Schools);
