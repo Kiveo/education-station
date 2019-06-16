8d74277ac6a41567cac1b2b43c4de2b9f4fc0a3a
Mav <mavericktesini@gmail.com>
2019-06-16 11:56:42 -0400

    adjust student enrolled status css

--- education-station/src/components/Student.js
+++ education-station/src/components/Student.js
@@ -2,16 +2,18 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const enrolledStyle = {
-  width: '1em', height: '1em', background: 'green', borderRadius: '50%', float: 'right', margin: '0 0.5em',
+  width: '1em', height: '1em', background: 'green', borderRadius: '50%', margin: '0 0.5em',
 };
 const notEnrolledStyle = {
-  width: '1em', height: '1em', background: 'red', borderRadius: '50%', float: 'right', margin: '0 0.5em',
+  width: '1em', height: '1em', background: 'red', borderRadius: '50%', margin: '0 0.5em',
 };
 
 const Student = ({ student }) => (
   <Fragment>
-    {student.Name}
-    {student.IsEnrolled ? <div style={enrolledStyle} /> : <div style={notEnrolledStyle} />}
+    <div className="flex-wrapper">
+      <p style={{ maxWidth: '70%' }}>{student.Name}</p>
+      {student.IsEnrolled ? <div style={enrolledStyle} /> : <div style={notEnrolledStyle} />}
+    </div>
     <hr style={{ height: '0.5em', background: '#FBA28D' }} />
   </Fragment>
 );
